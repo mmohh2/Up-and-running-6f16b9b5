b9f86f404e6452ead226d72198b439eea5abc075
Mo <moh19951@outlook.com>
2020-02-25 10:09:05 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'netland';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage("no connection"), (int)$e->getCode());
+}
+$moviedata = $pdo->query('select * from movies');
+
+
